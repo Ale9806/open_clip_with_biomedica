9c01fbe82133aecf6b8886febede7a6e26061056
Eduardo Alejandro Lozano Garcia <lozanoe@haic.stanford.edu>
2024-10-31 21:52:22 -0700

    modify script for hai

--- scripts/submit_and_manage_jobs_hai.py
+++ scripts/submit_and_manage_jobs_hai.py
@@ -4,10 +4,14 @@ import pandas as pd
 import uuid
 import socket
 
+from typing import Optional
+from huggingface_hub import HfApi, HfFolder
+
 # Constants
-GPU_TYPE="a100"
+URL_PATH = "/afs/.ir/users/l/o/lozanoe/repos/open_clip/scripts/urls.txt"
+GPU_TYPE="h100"
 NUM_NODES = 1
-NUM_GPUS = 2
+NUM_GPUS = 1
 MAX_RETRIES = 1
 TIME_TO_HANG = 1000
 error_dict = {
@@ -18,6 +22,39 @@ error_dict = {
 }
 
 
+def generate_tar_urls_from_repo(base_path: str, repo_id: str, url_path: str) -> None:
+    """
+    Retrieve .tar filenames from a Hugging Face dataset repository, filter them,
+    and generate a file with URLs for each .tar file in the repository. 
+
+    Each URL is formatted by prepending the base path to the filename, and 
+    URLs are joined with "::". The URLs are saved to a file named 'urls.txt'.
+
+    Args:
+        base_path (str): The base URL path to prepend to each filename.
+        repo_id (str): The ID of the Hugging Face repository (e.g., "username/repo_name").
+
+    Returns:
+        Optional[str]: The path to 'urls.txt' if URLs were successfully written to the file, 
+                       or None if no .tar files were found.
+    """
+    api = HfApi()
+    token = HfFolder.get_token()
+    
+    # Retrieve the file list from the specified repository
+    file_list = api.list_repo_files(repo_id=repo_id, repo_type="dataset", token=token)
+    tar_files = [file for file in file_list if file.endswith('.tar')]
+    
+    # If no .tar files are found, return None
+    if not tar_files:
+        return None
+    
+    # Generate URLs and write them to a file
+    urls = "::".join([f"{base_path}{filename}" for filename in tar_files])
+    with open(url_path, "w") as url_file:
+        url_file.write(urls)
+
+
 # Load job configurations from CSV
 def get_size_time(log_file_path):
     last_check_time = time.time()
@@ -26,6 +63,7 @@ def get_size_time(log_file_path):
     else:
         last_size = 0
     return last_check_time, last_size
+
 
 def check_errors_in_file(file_path):
     """
@@ -72,7 +110,12 @@ def find_free_port() -> int:
 # Function to submit and monitor a single job
 def submit_and_monitor_job(row, df):
 
-    os.makedirs('./slurm_logs', exist_ok=True)
+    # create url string text file
+    base_path = "https://huggingface.co/datasets/minwoosun/BioMedICA/resolve/main/"
+    repo_id = "minwoosun/BioMedICA"
+    generate_tar_urls_from_repo(base_path, repo_id, URL_PATH)
+    
+    os.makedirs('./slurm_logs_debug', exist_ok=True)
 
     # RDZV_ID = str(random.randint(0, 9999)) # trunkate to 4 digits
     # RDZV_PORT= random.randint(20000, 29999)  # Range from 20000 to 29999
@@ -84,17 +127,17 @@ def submit_and_monitor_job(row, df):
 
     with open(BATCH_SCRIPT, "w") as f:
         f.write(f"""#!/bin/bash
-#SBATCH --job-name=experiment
-#SBATCH -p pasteur
-#SBATCH -A pasteur
+#SBATCH --job-name=base
+#SBATCH -p hai
+#SBATCH -A cadd
 #SBATCH --nodes={NUM_NODES}
 #SBATCH --ntasks={NUM_NODES}
-#SBATCH --mem=64GB
+#SBATCH --mem=32GB
 #SBATCH --gres=gpu:{GPU_TYPE}:{NUM_GPUS}
 #SBATCH --cpus-per-task={2 * NUM_GPUS}
-#SBATCH --output=./slurm_logs/overfit-%j-out.txt
-#SBATCH --error=./slurm_logs/overfit-%j-err.txt
-#SBATCH --exclude=pasteur[1-4,8-9]
+#SBATCH --output=./slurm_logs_debug/base-%j-out.txt
+#SBATCH --error=./slurm_logs_debug/base-%j-err.txt
+#SBATCH --nodelist=haic-hgx-[2-5]
 
 nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
 nodes_array=($nodes)
@@ -111,33 +154,34 @@ export CUDA_VISIBLE_DEVICES=0
 export WANDB_RUN_GROUP="ViT_L_14_from_webli"
 
 source ~/.bashrc
-conda activate train_clip3
+conda activate train_clip
 
 cd ../src
 
-srun torchrun --nproc_per_node={NUM_GPUS} --nnodes={NUM_NODES} --node_rank=$SLURM_NODEID --rdzv_id {RDZV_ID} --rdzv_backend c10d --rdzv_endpoint $head_node_ip:{RDZV_PORT} -m open_clip_train.main \
+srun torchrun --nproc_per_node={NUM_GPUS} --nnodes={NUM_NODES} --node_rank=$SLURM_NODEID --rdzv_id {RDZV_ID} --rdzv_backend c10d --rdzv_endpoint $head_node_ip:{RDZV_PORT} -- \
+	-m open_clip_train.main \
     --save-most-recent \
-     --train-data "/pasteur2/u/ale9806/data/pmc-oa/full_panel_w_labels/commercial/{{000000..001263}}.tar::/pasteur2/u/ale9806/data/pmc-oa/full_panel_w_labels/noncommercial/{{000000..000533}}.tar::/pasteur2/u/ale9806/data/pmc-oa/full_panel_w_labels/other/{{000000..000110}}.tar" \
-    --train-num-samples 19060000 \
-    --accum-freq 4 \
+    --train-data {URL_PATH} \
+    --train-num-samples 15150000 \
+    --accum-freq 2 \
     --lr-scheduler "cosine" \
     --dataset-type "webdataset" \
-    --lr 1e-6 \
+    --lr 1e-7 \
     --beta1 0.9 \
     --beta2 0.95 \
     --warmup 1000 \
     --wd 0.2 \
-    --batch-size 2048 \
-    --epochs 100 \
+    --batch-size 128 \
+    --epochs 4 \
     --workers 1 \
     --model "ViT-L-14" \
-    --precision "fp16" \
+    --precision "fp32" \
     --local-loss \
     --gather-with-grad \
     --grad-checkpointing \
     --log-every-n-steps 1 \
     --seed 0 \
-    --logs ./logs_exp1/ \
+    --logs ./logs_exp_wds/ \
     --pretrained "commonpool_xl_clip_s13b_b90k" \
     --report-to "wandb" \
     --wandb-project-name "biomedica-ablation"
@@ -202,6 +246,9 @@ srun torchrun --nproc_per_node={NUM_GPUS} --nnodes={NUM_NODES} --node_rank=$SLUR
 
 
 if __name__=='__main__':
+
+    # hf authenticate
+
     df = pd.read_csv('experiments.csv')
     print("found", len(df), "jobs")
     threads = []
